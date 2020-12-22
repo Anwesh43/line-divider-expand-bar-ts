c8efe8a23a9ea47bdef724adcbdfc374dfd57e06
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 12:45:35 +0530

    adding Animator class

--- index.ts
+++ index.ts
@@ -121,4 +121,31 @@ class State {
             cb()
         }
     }
+
+    startUpdating(cb : Function) {
+        if (this.dir == 0) {
+            this.dir = 1 - 2 * this.prevScale 
+            cb()
+        }
+    }
+}
+
+class Animator {
+
+    animated : boolean = false 
+    interval : number 
+
+    start(cb : Function) {
+        if (!this.animated) {
+            this.animated = true 
+            this.interval = setInterval(cb, delay)
+        }
+    }
+
+    stop() {
+        if (this.animated) {
+            this.animated = false 
+            clearInterval(this.interval)
+        }
+    }
 }
