50c21687eaf22bd08a280906d8a0d65f9a9bb582
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 12:02:19 +0530

    adding project setup

---
+++ index.ts
@@ -0,0 +1,16 @@
+const w : number = window.innerWidth 
+const h : number = window.innerHeight 
+const parts : number = 5 
+const scGap : number = 0.02 / parts 
+const strokeFactor : number = 90 
+const sizeFactor : number = 12.9 
+const barFactor : number = 5.9 
+const delay : number = 20 
+const colors : Array<string> = [
+    "#F44336",
+    "#3F51B5",
+    "#4CAF50",
+    "#FF9800",
+    "#795548"
+]
+const backColor : string = "#BDBDBD"
