d5f5b8ba9caa1ffda35080f3b8b822a7d9a8e742
Anwesh43 <anweshthecool0@gmail.com>
2020-12-22 12:19:55 +0530

    adding DrawingUtil class

--- index.ts
+++ index.ts
@@ -29,3 +29,48 @@ class ScaleUtil {
         return Math.sin(scale * Math.PI)
     }
 }
+
+class DrawingUtil {
+
+    static drawLine(context : CanvasRenderingContext2D, x1 : number, y1 : number, x2 : number, y2 : number) {
+        context.beginPath()
+        context.moveTo(x1, y1)
+        context.lineTo(x2, y2)
+        context.stroke()
+    }
+
+    static drawLineDivideExpand(context : CanvasRenderingContext2D, scale : number) {
+        const size : number = Math.min(w, h) / sizeFactor 
+        const sf : number = ScaleUtil.sinify(scale)
+        const sf1 : number = ScaleUtil.divideScale(sf, 0, parts)
+        const sf2 : number = ScaleUtil.divideScale(sf, 1, parts)
+        const sf3 : number = ScaleUtil.divideScale(sf, 2, parts)
+        const sf4 : number = ScaleUtil.divideScale(sf, 3, parts)
+        const barSize : number = Math.min(w, h) / barFactor 
+        context.save()
+        context.translate(w / 2, h / 2)
+        for (var j = 0; j < 2; j++) {
+            const sj : number = 1 - 2 * j 
+            context.save()
+            context.scale(sj, 1)
+            for (var k = 0; k < 2; k++) {
+                context.save()
+                context.scale(1, 1 - 2 * k)
+                context.translate((w / 2 - size) * sf2, barSize * sf3)
+                DrawingUtil.drawLine(context, 0, 0, size * sf1, 0)
+                context.fillRect(0, -(barSize) * sf4, size, barSize * sf4)
+                context.restore()
+            }
+            context.restore()
+        }
+        context.restore()
+    }
+    
+    static drawLDENode(context : CanvasRenderingContext2D, i : number, scale : number) {
+        context.lineCap = 'round'
+        context.lineWidth = Math.min(w, h) / strokeFactor 
+        context.strokeStyle = colors[i]
+        context.fillStyle = colors[i]
+        DrawingUtil.drawLineDivideExpand(context, scale)
+    }
+}
